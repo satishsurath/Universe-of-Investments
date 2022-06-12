0a9d58a8ae4481ec15255cf8c6624dbc7061cfea
Satish Surath <s@n-m.co>
2022-06-12 11:39:43 -0400

    Cleaning up routes.py and adding technical indicators functions
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/routes.py
+++ app/routes.py
@@ -32,7 +32,7 @@ import yfinance as yf
 import os
 import requests
 #Importing MCForecast Tool
-from MCForecastTools import MCSimulation
+#from MCForecastTools import MCSimulation
 
 debugtoggle = True
 # import alpaca_trade_api as tradeapi #####Commenting the deprecated way to call the API
@@ -63,16 +63,38 @@ def cb2(endpoint):
         stock = request.args.get('data')
         st = yf.Ticker(stock)
         return json.dumps(st.info)
-    elif endpoint == "getReturn":
-        return mcforecast_get_data(request.args.get('data'),request.args.get('period'),request.args.get('interval'))
-    elif endpoint == "getPortfolio":
-        return mcforecast_get_portfolio(request.args.get('stock1'),request.args.get('stock2'),request.args.get('stock3'),request.args.get('stock4'),request.args.get('stock5'))
-    elif endpoint == "getCumulativeReturn":
-        return mcforecast_get_portfolio_cumulative_return(request.args.get('stock1'),request.args.get('stock2'),request.args.get('stock3'),request.args.get('stock4'),request.args.get('stock5'))
     else:
         return "Bad endpoint", 400
     
+
     
+#function to calculate the RSI Technical Indicator
+def RSI(df, n=14):
+    "function to calculate RSI"
+    df = df.copy()
+    df["change"] = df["Adj Close"] - df["Adj Close"].shift(1)
+    df["gain"] = np.where(df["change"]>=0, df["change"], 0)
+    df["loss"] = np.where(df["change"]<0, -1*df["change"], 0)
+    df["avgGain"] = df["gain"].ewm(alpha=1/n, min_periods=n).mean()
+    df["avgLoss"] = df["loss"].ewm(alpha=1/n, min_periods=n).mean()
+    df["rs"] = df["avgGain"]/df["avgLoss"]
+    df["rsi"] = 100 - (100/ (1 + df["rs"]))
+    return df
+
+#function stochastic_oscillator
+def add_stochastic_oscillator(df, periods=14):
+    copy = df.copy()
+    high_roll = copy["High"].rolling(periods).max()
+    low_roll = copy["Low"].rolling(periods).min()
+    # Fast stochastic indicator
+    num = copy["Adj Close"] - low_roll
+    denom = high_roll - low_roll
+    copy["K"] = (num / denom) * 100
+    # Slow stochastic indicator
+    copy["D"] = copy["K"].rolling(3).mean()   
+    return copy
+#dfso = add_stochastic_oscillator(df, periods=14)
+
 # Return the JSON data for the Plotly graph
 def gm(stock,period, interval):
     st = yf.Ticker(stock)
@@ -118,108 +140,6 @@ def alpaca_get_market_data(stock,period, interval):
         range_y=(min,max), template="seaborn", title=chart_title)
     graphJSON = json.dumps(fig_stock, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
-
-
-def mcforecast_get_data(stock,period, interval):
-    start_date = "2019-04-10"
-    end_date = "2022-04-10"
-    # Set the tickers
-    # tickers = "AAPL"
-    tickers = stock
-    timeframe = "1D"
-    api = REST(api_key1, api_secret_key1, api_version='v2')
-    df2 = api.get_bars(tickers, TimeFrame.Day, start_date, end_date, adjustment='raw').df
-    df2.loc[:,'symbol'] = tickers
-    pivottable = pd.pivot_table(df2, values='close', index=df2.index, columns=['symbol'])
-    ticker_name = [(x,'close') for x in pivottable.columns]
-    micolumns = pd.MultiIndex.from_tuples(ticker_name)
-    pivottable.columns = micolumns
-    chart_title = "Percentage Change for " + stock
-    MC_5years = MCSimulation(portfolio_data = pivottable,
-        weights = [1],
-        num_simulation = 100,
-        num_trading_days = 252*5
-    )
-    
-
-    micolumns = ['close','daily_return']
-    MC_5years.portfolio_data.columns = micolumns
-    max = (MC_5years.portfolio_data['daily_return'].max())
-    min = (MC_5years.portfolio_data['daily_return'].min())
-    range = max - min
-    margin = range * 0.05
-    max = max + margin
-    min = min - margin
-    
-    fig_stock = px.area(MC_5years.portfolio_data, x=MC_5years.portfolio_data.index, y='daily_return', 
-        range_y=(min,max), template="seaborn", title=chart_title)
-    graphJSON = json.dumps(fig_stock, cls=plotly.utils.PlotlyJSONEncoder)
-    return graphJSON
-
-def mcforecast_get_portfolio(stock1,stock2,stock3,stock4,stock5):
-    start_date = "2019-04-10"
-    end_date = "2022-04-10"
-    # Set the tickers
-    # tickers = "AAPL"
-    tickers = [stock1,stock2,stock3,stock4,stock5]
-    timeframe = "1D"
-    api = REST(api_key1, api_secret_key1, api_version='v2')
-    df2 = api.get_bars(tickers, TimeFrame.Day, start_date, end_date, adjustment='raw').df
-    pivottable = pd.pivot_table(df2, values='close', index=df2.index, columns=['symbol'])
-    ticker_name = [(x,'close') for x in pivottable.columns]
-    micolumns = pd.MultiIndex.from_tuples(ticker_name)
-    pivottable.columns = micolumns
-    #chart_title = "Montecarlo Simulations for " + tickers[0] + ";" + tickers[1] + ";" + tickers[2] + ";" + tickers[3] + ";" + tickers[4] + ";"
-    MC_5years = MCSimulation(portfolio_data = pivottable,
-        weights = [0.2,0.2,0.2,0.2,0.2],
-        num_simulation = 100,
-        num_trading_days = 252*5
-    )
-    MC_5years.calc_cumulative_return()    
-    plot_title = f"{MC_5years.nSim} Simulations of Cumulative Portfolio Return Trajectories Over the Next {MC_5years.nTrading} Trading Days."
-    fig_stock = px.line(MC_5years.simulated_return, template="seaborn", title=plot_title)
-    fig_stock.update_layout(showlegend=False)
-    graphJSON = json.dumps(fig_stock, cls=plotly.utils.PlotlyJSONEncoder)
-    return graphJSON
-
-def mcforecast_get_portfolio_cumulative_return(stock1,stock2,stock3,stock4,stock5):
-    start_date = "2019-04-10"
-    end_date = "2022-04-10"
-    # Set the tickers
-    # tickers = "AAPL"
-    tickers = [stock1,stock2,stock3,stock4,stock5]
-    timeframe = "1D"
-    api = REST(api_key1, api_secret_key1, api_version='v2')
-    df2 = api.get_bars(tickers, TimeFrame.Day, start_date, end_date, adjustment='raw').df
-    pivottable = pd.pivot_table(df2, values='close', index=df2.index, columns=['symbol'])
-    ticker_name = [(x,'close') for x in pivottable.columns]
-    micolumns = pd.MultiIndex.from_tuples(ticker_name)
-    pivottable.columns = micolumns
-    #chart_title = "Montecarlo Simulations for " + tickers[0] + ";" + tickers[1] + ";" + tickers[2] + ";" + tickers[3] + ";" + tickers[4] + ";"
-    MC_5years = MCSimulation(portfolio_data = pivottable,
-        weights = [0.2,0.2,0.2,0.2,0.2],
-        num_simulation = 100,
-        num_trading_days = 252*5
-    )
-    MC_5years.calc_cumulative_return()    
-    initial_investment = 1000
-    # Use the lower and upper `95%` confidence intervals to calculate the range of the possible outcomes of our $20,000
-    ci_lower = round(MC_5years.confidence_interval.iloc[0] * initial_investment, 2)
-    ci_upper = round(MC_5years.confidence_interval.iloc[1] * initial_investment, 2)
-    # Print results
-    data = {}
-    cumulative_return = (f"Using <span class='maindiv'>"
-                f"<span style='text-decoration: underline;background:#d4dbef;' data-hover=''> Monte-Carlo Simulation, </span> <span class='test'>Monte Carlo simulation (also known as the Monte Carlo Method) lets you see all the possible outcomes of your decisions and assess the impact of risk.<a href='/Educate-Yourself#Monte-Carlo' target='_blank'>Read More</a></span></span>" 
-        f" with 95%  <span class='maindiv'>"
-                f"<span style='text-decoration: underline;background:#d4dbef;' data-hover=''> confidence, </span> <span class='test'>Learn about the value at risk, how confidence intervals and confidence levels are used to interpret the value at risk and the difference between the two.<a href='/Educate-Yourself#Confidence' target='_blank'>Read More</a></span></span>  "
-        f" the projected <span class='maindiv'><span style='text-decoration: underline;background:#d4dbef;' data-hover=''> return, </span> <span class='test'>Return is the financial gain or loss on an investment. Yield measures the income, such as interest and dividends, from an investment and is expressed as a percentage.<a href='/Educate-Yourself#Return' target='_blank'>Read More</a></span></span>  on an initial investment of ${initial_investment} in the portfolio"
-      f" over the next 5 years will fall within in the range of"
-      f" ${ci_lower:,} and ${ci_upper:,}"
-      f"<br><br> This site is for educational and demonstation purposes, only and should not be relied upon as Finanacial Advice")    
-    data['key'] = cumulative_return
-    json_data = json.dumps(data)
-    return json_data
-
 
 
 @app.route('/Stock')
